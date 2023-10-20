dd5b3f844b3341a9e178eb218e08e4407e1e5331
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-20 20:04:14 +0500

    Task1.V9

---
+++ Tyuiu.ZakharovaYV.Sprint2.Task1.V9.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+//Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность можно чередовать, но использовать один раз в выражении)
+//и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться), а также арифметических выражений,
+//которая вернет логическую последовательность(массив): (False, False, True, False, True, False), при a = 15, b = 335, c = 174, d = 478
+
+//Название проектов (консольного приложения, библиотеки классов, тестового модуля) оформить по шаблону в соответствии с вариантом.
+//Интерфейс консольного приложения оформить по шаблону. 
+
+
+
+namespace Tyuiu.ZakharovaYV.Sprint2.Task1.V9.Lib
+{
+    public class DataService : ISprint2Task1V9
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c > d);    //f + f = f
+            res[1] = (a + 3 > b) & (c + 300 < d); //f+f=f
+            res[2] = (a > b) || (c < d); //f+t=t
+            res[3] = (a+2>b) && (c < d); //f + t = f
+            res[4] = (!res[0]); //t
+            res[5] = (a > b) ^ (c > d); //f+f =f
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaYV.Sprint2.Task1.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZakharovaYV.Sprint2.Task1.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZakharovaYV.Sprint2.Task1.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b387ffd4-8f28-4430-989a-f1fa9863bf9b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
